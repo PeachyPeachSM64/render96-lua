ddad5a36d9d2499633d62de2c2e811ea274dae01
Squishy <squishy6094@gmail.com>
2026-05-06 20:46:54 -0700

    Add vanilla mario as a CS option

--- render96_A/characters.lua
+++ render96_A/characters.lua
@@ -1,6 +1,17 @@
 local r96lib = require("/lib/r96lib")
 require("constants")
 
+-- Config Character Select to add Vanilla models
+local E_MODEL_R96_MARIO = smlua_model_util_get_id("r96_mario_geo")
+_G.charSelect.character_add_costume(CT_MARIO, "Vanilla Mario", nil, nil, nil, E_MODEL_MARIO)
+_G.charSelect.character_edit(CT_MARIO, nil, nil, "Render96", nil, E_MODEL_R96_MARIO)
+
+--[[
+local E_MODEL_R96_WARIO = smlua_model_util_get_id("r96_wario_geo")
+_G.charSelect.character_add_costume(CT_WARIO, "Vanilla Wario", nil, nil, nil, E_MODEL_WARIO)
+_G.charSelect.character_edit(CT_WARIO, nil, nil, "Render96", nil, E_MODEL_R96_WARIO)
+]]
+
 eyeStateCustom = {
     EYES_OPEN = 0,
     EYES_HALF_CLOSED = 1,
